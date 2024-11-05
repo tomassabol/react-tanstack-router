fc532141018d743ae22b15434b38409a40e34963
Tomáš Sabol <tomassabol2@gmail.com>
2024-11-05 15:08:25 +0100

    first commit

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -10,104 +10,104 @@
 
 // Import Routes
 
-import { Route as rootRoute } from './routes/__root'
-import { Route as ViteImport } from './routes/vite'
-import { Route as ReactImport } from './routes/react'
-import { Route as IndexImport } from './routes/index'
+import { Route as rootRoute } from "./routes/__root";
+import { Route as ViteImport } from "./routes/vite";
+import { Route as ReactImport } from "./routes/react";
+import { Route as IndexImport } from "./routes/index";
 
 // Create/Update Routes
 
 const ViteRoute = ViteImport.update({
-  id: '/vite',
-  path: '/vite',
+  id: "/vite",
+  path: "/vite",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const ReactRoute = ReactImport.update({
-  id: '/react',
-  path: '/react',
+  id: "/react",
+  path: "/react",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const IndexRoute = IndexImport.update({
-  id: '/',
-  path: '/',
+  id: "/",
+  path: "/",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 // Populate the FileRoutesByPath interface
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexImport
-      parentRoute: typeof rootRoute
-    }
-    '/react': {
-      id: '/react'
-      path: '/react'
-      fullPath: '/react'
-      preLoaderRoute: typeof ReactImport
-      parentRoute: typeof rootRoute
-    }
-    '/vite': {
-      id: '/vite'
-      path: '/vite'
-      fullPath: '/vite'
-      preLoaderRoute: typeof ViteImport
-      parentRoute: typeof rootRoute
-    }
+    "/": {
+      id: "/";
+      path: "/";
+      fullPath: "/";
+      preLoaderRoute: typeof IndexImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/react": {
+      id: "/react";
+      path: "/react";
+      fullPath: "/react";
+      preLoaderRoute: typeof ReactImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/vite": {
+      id: "/vite";
+      path: "/vite";
+      fullPath: "/vite";
+      preLoaderRoute: typeof ViteImport;
+      parentRoute: typeof rootRoute;
+    };
   }
 }
 
 // Create and export the route tree
 
 export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
-  '/react': typeof ReactRoute
-  '/vite': typeof ViteRoute
+  "/": typeof IndexRoute;
+  "/react": typeof ReactRoute;
+  "/vite": typeof ViteRoute;
 }
 
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
-  '/react': typeof ReactRoute
-  '/vite': typeof ViteRoute
+  "/": typeof IndexRoute;
+  "/react": typeof ReactRoute;
+  "/vite": typeof ViteRoute;
 }
 
 export interface FileRoutesById {
-  __root__: typeof rootRoute
-  '/': typeof IndexRoute
-  '/react': typeof ReactRoute
-  '/vite': typeof ViteRoute
+  __root__: typeof rootRoute;
+  "/": typeof IndexRoute;
+  "/react": typeof ReactRoute;
+  "/vite": typeof ViteRoute;
 }
 
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/react' | '/vite'
-  fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/react' | '/vite'
-  id: '__root__' | '/' | '/react' | '/vite'
-  fileRoutesById: FileRoutesById
+  fileRoutesByFullPath: FileRoutesByFullPath;
+  fullPaths: "/" | "/react" | "/vite";
+  fileRoutesByTo: FileRoutesByTo;
+  to: "/" | "/react" | "/vite";
+  id: "__root__" | "/" | "/react" | "/vite";
+  fileRoutesById: FileRoutesById;
 }
 
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute
-  ReactRoute: typeof ReactRoute
-  ViteRoute: typeof ViteRoute
+  IndexRoute: typeof IndexRoute;
+  ReactRoute: typeof ReactRoute;
+  ViteRoute: typeof ViteRoute;
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   ReactRoute: ReactRoute,
   ViteRoute: ViteRoute,
-}
+};
 
 export const routeTree = rootRoute
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>()
+  ._addFileTypes<FileRouteTypes>();
 
 /* ROUTE_MANIFEST_START
 {
